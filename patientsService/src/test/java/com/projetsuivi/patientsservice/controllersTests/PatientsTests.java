29f7844ff6ec6826b8d29e4dfb342a17de3bd4fe
Nobound <jean.ungerer.nobound@gmail.com>
2024-01-14 13:29:25 +0100

    [evo] config for docker and cleansing commented code

--- patientsService/src/test/java/com/projetsuivi/patientsservice/controllersTests/PatientsTests.java
+++
@@ -1,9 +0,0 @@
-package com.projetsuivi.patientsservice.controllersTests;
-
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-
-@SpringBootTest
-@ActiveProfiles("test")
-public class PatientsTests {
-}
